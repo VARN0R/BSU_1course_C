b2c0117579cfbfa77c4151d49b387bbdabda4ba2
Vladislav <118449746+VARN0R@users.noreply.github.com>
2023-04-19 20:28:24 +0300

    update

---
+++ 2sem/OOP/laba1.3/main.cpp
@@ -0,0 +1,83 @@
+/*
+Написать программу для работы с базой данных, содержащей записи со све-
+дениями о студентах: ФИО, возраст, пол, курс, успеваемость, в которой должны
+выполняться следующие действия: создание файла, просмотр файла
+Выполнить задания для бинарного файла. Размер файлов <= 64GiB.
+
+По исходному файлу определить всех отличников. Сформировать файл из
+этих студентов.
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+struct Students {
+    char name[20];
+    int age;
+    char female;
+    int cource;
+    int marks;    
+};
+
+void StudentInFile(int &amount){
+    ofstream fout("source.bin", ios_base::binary);
+    for(int i = 0; i < amount; i++){
+        Students temp_person;
+        cout << "Enter name: ";
+        cin >> temp_person.name;
+        cout << "Enter female: ";
+        cin >> temp_person.female;
+        cout << "Enter cource: ";
+        cin >> temp_person.cource;
+        cout << "Enter marks: ";
+        cin >> temp_person.marks;
+        cout << endl;
+        fout.write(reinterpret_cast<char*>(&temp_person), sizeof(Students));
+    }
+    fout.close();
+}
+
+void StudentOutFile(){
+    ifstream fin("sorted.bin", ios_base::binary);
+    Students temp_person;
+    while(fin.read((char*)&temp_person, sizeof(Students))){
+        cout << "NAME: "<< temp_person.name << endl;
+        cout << "FEMALE: "<< temp_person.female << endl;
+        cout << "COURCE: "<< temp_person.cource << endl;
+        cout << "MARKS: "<< temp_person.marks << endl;
+        cout << endl;
+    }
+    fin.close();
+}
+
+void StudentSort(){
+    ifstream fin("source.bin", ios_base::binary);
+    ofstream fout("sorted.bin", ios_base::binary);
+    Students temp_person;
+    cout << "Enter good mark: ";
+    int good_mark;
+    cin >> good_mark;
+    while(fin.read((char*)&temp_person, sizeof(Students))){
+        if (temp_person.marks >= good_mark){
+            fout.write((char*)&temp_person, sizeof(Students));
+        }
+        else {
+            continue;
+        }
+    }
+    fin.close();
+    fout.close();
+}
+
+int main(){
+    int amount{};
+    cout << "Enter amount students: ";
+    cin >> amount;
+    StudentInFile(amount);
+    StudentSort();
+    StudentOutFile();
+    return 0;
+}
---
+++ 2sem/OOP/laba1.4/main.cpp
@@ -0,0 +1,90 @@
+/*
+Написать программу для работы с базой данных, содержащей записи со све-
+дениями о студентах: ФИО, возраст, пол, курс, успеваемость, в которой должны
+выполняться следующие действия: создание файла, просмотр файла
+Выполнить задания для бинарного файла. Размер файлов <= 64GiB.
+
+По исходному файлу определить всех отличников. Сформировать файл из
+этих студентов.
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+struct Students {
+    char name[20];
+    int age;
+    char female;
+    int cource;
+    int marks;
+};
+
+void StudentInFile(int &amount){//функция для ввода студентов в файл
+    ofstream fout("source.txt"); 
+    for(int i = 0; i < amount; i++){
+        Students temp_person; // временная перемена в которую мы будем класть данные текущего студента
+        cout << "Enter name: ";
+        cin >> temp_person.name;
+        fout << temp_person.name << " ";
+        cout << "Enter female: ";
+        cin >> temp_person.female;
+        fout << temp_person.female << " ";
+        cout << "Enter cource: ";
+        cin >> temp_person.cource;
+        fout << temp_person.cource << " ";
+        cout << "Enter marks: ";
+        cin >> temp_person.marks;
+        fout << temp_person.marks << " ";
+        cout << endl;
+    }
+    fout.close();
+}
+
+void StudentOutFile(){ // функция для вывода студентов из файла на экран
+    ifstream fin("sorted.txt");
+    Students temp_person;// опять же временная переменная для хранения данных о текущем студенте
+    while(fin >> temp_person.name >> temp_person.female >> temp_person.cource >> temp_person.marks){ //считываем данные с файла в эту переменную
+        //выводим все что считали на экран
+        cout << "NAME: "<< temp_person.name << endl;
+        cout << "FEMALE: "<< temp_person.female << endl;
+        cout << "COURCE: "<< temp_person.cource << endl;
+        cout << "MARKS: "<< temp_person.marks << endl;
+        cout << endl;
+    }
+    fin.close();
+}
+
+void StudentSort(){//функция для сортировки студентов в зависимости от их оценки
+    ifstream fin("source.txt");
+    ofstream fout("sorted.txt");
+    Students temp_person;
+    cout << "Enter good mark: ";
+    int good_mark;
+    cin >> good_mark;
+    while(fin >> temp_person.name >> temp_person.female >> temp_person.cource >> temp_person.marks){ 
+        if (temp_person.marks >= good_mark){
+            fout << temp_person.name << " ";
+            fout << temp_person.female << " ";
+            fout << temp_person.cource << " ";
+            fout << temp_person.marks << " ";
+        }
+        else {
+            continue;
+        }
+    }
+    fin.close();
+    fout.close();
+}
+
+int main(){
+    int amount{};
+    cout << "Enter amount students: ";
+    cin >> amount;
+    StudentInFile(amount);
+    StudentSort();
+    StudentOutFile();
+    return 0;
+}
