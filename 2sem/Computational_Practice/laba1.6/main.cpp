2a4ceb0a6f5fbfc79bac284ce32122f4f77805d2
Vladislav <118449746+VARN0R@users.noreply.github.com>
2023-06-24 23:00:05 +0300

    update

--- 2sem/Computational_Practice/laba1.6/main.cpp
+++ 2sem/Computational_Practice/laba1.6/main.cpp
@@ -1,17 +1,32 @@
+/*
+TASK CONDITION
+-------------------------------------------------------------------------------
+Дан массив чисел произвольной длины. Отсортировать массив, используя сортировку слияниями.
+Создать шаблоны и перегрузки функций для сортировки, вывода результатов, заполнения массивов.
+Выполнить сортировки массивов для данных разных типов: целых, вещественных, символьных, С- строк, 
+используя написанные функции.
+-------------------------------------------------------------------------------
+ENGLISH
+An array of numbers of arbitrary length is given. Sort the array using merge sorting.
+Create templates and function overloads for sorting, output results, and filling arrays.
+Perform array sorting for data of different types: integer, real, character, C-strings, 
+using written functions.
+-------------------------------------------------------------------------------
+*/
 #include "laba1.6.h"
 using namespace std;
-
 int main() {
 	cout << "Max element: ";
 	int N;	
 	cin >> N;
 	int* Array = new int [N];//char *Array = new char [N]; 
-    for (int i = 0; i < N; i++){
-        cin >> Array[i];
-    }
+
+	inputArray(Array, N);
 	mergeSort(Array, 0, N-1);
 	OutputArray(Array, N);
 	delete [] Array;
 	return 0;	
 }
+
+
 
