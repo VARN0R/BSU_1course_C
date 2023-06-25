3ef7977b210d5f995c1e81f558103527c0855e70
Vladislav <118449746+VARN0R@users.noreply.github.com>
2023-06-25 21:06:02 +0300

    update

--- 2sem/OOP/laba3.2/laba3.2.2/header.h
+++ 2sem/OOP/laba3.2/laba3.2.2/header.h
@@ -2,12 +2,7 @@
 #include <iostream>
 #include <vector>
 
-
-
 void inputMatrix(std::vector<std::vector<double>>&Matrix, int rows, int columns);
-
 void outputMatrix(std::vector<std::vector<double>>&Matrix, int rows, int columns);
-
 void Multiplay(std::vector<std::vector<double>>&Matrix, int rows, std::vector<std::vector<double>>&Matrix2, int columns);
-
 void Exponentation(std::vector<std::vector<double>>&Matrix, int rows, int columns);
